5ac57ea9f685664d08388185f511bbcc3e8cf9ab
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-26 00:59:28 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -2,15 +2,16 @@
 #make sure to edit the value of each key to replace < path to this directory >
 #with the correct path to this directory on your computer
 
-# my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
-# }
+my_songs = {
+"Go Go GO" => '/Users/sjami002/jukebox-cli-dumbo-web-career-040119/audio/Emerald-Park/01.mp3',
+"LiberTeens" => '/Users/sjami002/jukebox-cli-dumbo-web-career-040119/audio/Emerald-Park/02.mp3',
+"Hamburg" =>  '/Users/sjami002/jukebox-cli-dumbo-web-career-040119/audio/Emerald-Park/03.mp3',
+"Guiding Light" => '/Users/sjami002/jukebox-cli-dumbo-web-career-040119/audio/Emerald-Park/04.mp3',
+"Wolf" => '/Users/sjami002/jukebox-cli-dumbo-web-career-040119/audio/Emerald-Park/05.mp3',
+"Blue" => '/Users/sjami002/jukebox-cli-dumbo-web-career-040119/audio/Emerald-Park/06.mp3',
+"Graduation Failed" => '/Users/sjami002/jukebox-cli-dumbo-web-career-040119/audio/Emerald-Park/07.mp3'
+}
+
 
 def help
   #this method should be the same as in jukebox.rb
@@ -20,8 +21,9 @@ end
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
+  #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
+
 end
 
 
@@ -33,13 +35,16 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+
 end
 
 def exit_jukebox
   #this method is the same as in jukebox.rb
+
 end
 
 def run(my_songs)
   #this method is the same as in jukebox.rb
+  
 end
+run(my_songs)
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,67 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  #this method should be the same as in jukebox.rb
+
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits the program"
+end
+
+def list(my_songs)
+  #this method is different! Collect the keys of the my_songs hash and
+  #list the songs by name
+  my_songs.keys.each do |song|
+   puts song
+ end
+end
+
+def play(my_songs)
+  #this method is slightly different!
+  #you should still ask the user for input and collect their song choice
+  #this time, only allow user's to input a song name
+  #check to see if the name they give is in fact a key of the my_songs hash
+  #if it isn't, tell them their choice is invalid
+  #if it is, play the song using the system 'open <file path>' syntax
+  #get the file path of the song by looking it up in the my_songs hash
+
+  puts "Please enter a song name"
+  user = gets.chomp
+  if my_songs.keys.include?(user)
+    song = my_songs[user]
+    system "open #{song}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  #this method is the same as in jukebox.rb
+  puts "Goodbye"
+end
+
+def run(my_songs)
+  #this method is the same as in jukebox.rb
+  help
+  while true
+    puts "Please enter a command:"
+    command = gets.chomp
+    case command
+      when "list"
+        list(songs)
+      when "play"
+        play(songs)
+      when "help"
+        help
+      when "exit"
+        exit_jukebox
+        break
+      else
+        help
+    end
+  end
+end
+run(my_songs)
